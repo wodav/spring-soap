131f04226556f6451e35172bd48693b593873673
wodav <74779169+wodav@users.noreply.github.com>
2024-11-15 15:46:05 +0100

    initial commit

---
+++ src/test/java/wodav/spring_soap/SpringSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package wodav.spring_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
