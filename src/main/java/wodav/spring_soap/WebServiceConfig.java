0ad1bca62db2cd6ed00b4c6b2b2593f99278da30
wodav <74779169+wodav@users.noreply.github.com>
2024-11-15 16:23:14 +0000

    feature/generate-java-from-xsd: Update first soap endpoint gen path

--- src/main/java/wodav/spring_soap/WebServiceConfig.java
+++ src/main/java/wodav/spring_soap/WebServiceConfig.java
@@ -29,7 +29,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("CountriesPort");
         wsdl11Definition.setLocationUri("/ws");
-        wsdl11Definition.setTargetNamespace("http://www.baeldung.com/springsoap/gen");
+        wsdl11Definition.setTargetNamespace("wodav/spring-soap/gen");
         wsdl11Definition.setSchema(countriesSchema);
         return wsdl11Definition;
     }
